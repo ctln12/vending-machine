968c35be9fac692cf01b0f8787b0a4a539658732
Cataline Baert <snuf_kin@yahoo.com>
2021-01-31 22:26:51 +0100

    add style to links and components

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import './VendingMachine.css';
 
 class VendingMachine extends Component {
   render() {
@@ -7,7 +8,7 @@ class VendingMachine extends Component {
       <div className="VendingMachine">
         <h1>Welcome to the Vending Machine!</h1>
         <p>Please choose a product</p>
-        <div>
+        <div className="VendingMachine-items">
           <Link to="/soda">Soda</Link>
           <Link to="/chips">Chips</Link>
           <Link to="/candies">Candies</Link>
